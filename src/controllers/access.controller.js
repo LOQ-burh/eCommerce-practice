c3029622efcf53edc930c53e666b27a4797fea34
LOQ-burh <nguyenminhquan042004@gmail.com>
2024-08-13 19:11:47 +0700

    Editing the API response can use class

--- src/controllers/access.controller.js
+++ src/controllers/access.controller.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const { OK, CREATED } = require("../core/success.response")
 const AccessService = require("../services/access.service")
 
 class AccessController {
@@ -7,8 +8,13 @@ class AccessController {
   signUp = async (req, res, next) => {
     // 200 OK
     // 201 CREATED
+    new CREATED({
+      message: 'Register OK!',
+      metadata: await AccessService.signUp(req.body),
+      option: 'abc'
+    }).send(res)
 
-    return res.status(201).json(await AccessService.signUp(req.body))
+    // return res.status(201).json(await AccessService.signUp(req.body))
   }
 }
 module.exports = new AccessController()
